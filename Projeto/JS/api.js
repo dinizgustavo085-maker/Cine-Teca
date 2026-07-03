e86a8807a00e8f4c5917f61400a0c7f9c199be89
Gustavo Diniz <dinizgustavo085@gmail.com>
2026-07-03 00:49:46 -0300

    comentando código e adicionando imagem as informações da api

--- Projeto/JS/api.js
+++ Projeto/JS/api.js
@@ -2,15 +2,17 @@ let chaveapic = "9b87dd9b"
 
 function busca() {
     let titulo = document.getElementById("titulo").value;
-
+    // verifica se o campo de título está vazio e retorna caso esteja
     if (titulo === "") {
         return;
     }
+    // faz a requisição para a API do OMDB usando o método fetch e passando o título do filme como parâmetro
     fetch(`https://www.omdbapi.com/?apikey=${chaveapic}&s=${titulo}`)
         .then(function (resposta) {
             return resposta.json();
         })
 
+        // converte a resposta da API em um objeto JavaScript
         .then(function (dados) {
             if (dados.erro) {
                 alert("Dados do filme não encontrado")
@@ -18,16 +20,22 @@ function busca() {
             }
 
             console.log(dados.Search);
-            
-            for (let index = 0; index < dados.Search.length; index++) {               
+            // percorre o array de filmes retornado pela API e cria elementos HTML para exibir as informações de cada filme
+            for (let index = 0; index < dados.Search.length; index++) {
+                // seleciona o elemento HTML onde os filmes serão exibidos               
                 let filmes = document.getElementById("lista-filmes");
+
+                // cria um elemento div para cada filme e adiciona as informações do filme dentro dele
                 let sessaoFilmes = document.createElement("div");
 
+                // Adiciona a classe "filme" à div criada
                 sessaoFilmes.innerHTML =
+                    // Adiciona as informações do filme dentro da div
                     "<p><strong>Nome: </strong> " + dados.Search[index].Title + "</p>"+
                     "<p><strong>Ano: </strong> " + dados.Search[index].Year + "</p>" + 
                     "<p><strong>Type: </strong> " + dados.Search[index].Type + "</p>"+
-                    "<p><strong>Poster: </strong> " + dados.Search[index].Poster + "</p>"
+                    "<img src='" + dados.Search[index].Poster + "' alt='Poster do Filme' widtha='150'>"
+                // Adiciona a div criada ao elemento HTML selecionado    
                 filmes.appendChild(sessaoFilmes);
             }
 
@@ -35,5 +43,4 @@ function busca() {
         })
 }
 
-const campo = document.getElementById("titulo");
 
